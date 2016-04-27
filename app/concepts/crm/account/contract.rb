a62f8875148219d03e59b8ac5939253045365032
panigrah <panigrahi@zoho.com>
2016-04-27 21:23:12 +0800

    fixed validation tests

--- app/concepts/crm/account/contract.rb
+++ app/concepts/crm/account/contract.rb
@@ -34,7 +34,7 @@ module CRM
 
       validation :default do
         required(:name) { filled? & unique? }   
-        optional(:rating).maybe(inclusion?: %w(0 1 2 3 4 5))
+        optional(:rating).maybe(inclusion?: [0,1,2,3,4,5])
         optional(:category).maybe(inclusion?: Setting.unroll(:account_category).map { |s| s.last.to_s })	
 
         configure do
--- spec/concepts/account/operation_spec.rb
+++ spec/concepts/account/operation_spec.rb
@@ -3,9 +3,9 @@ require 'spec_helper'
 describe CRM::Account::Create do
   describe "create an account with valid parameters" do
     it "should create a new account given valid attributes" do
-      op = CRM::Account::Create.(account: {name: 'Awesome!', rating: 4})
-      op.model.name.should eq("Awesome!")
-      op.model.persisted?.should eq(true)
+      op = CRM::Account::Create.(account: {name: 'Awesome!'})
+      expect(op.model.name).to eq("Awesome!")
+      expect(op.model.persisted?).to eq(true)
     end
   end
 
@@ -14,17 +14,17 @@ describe CRM::Account::Create do
       expect{op = CRM::Account::Create.(account: {name: nil})}.to raise_error(Trailblazer::Operation::InvalidContract)
     end
 
-    it "should raise a Trailblazer::Opration::InvalidContract on create if an account with the same name exists" do
-        op1 = CRM::Account::Create.(account: {name: 'Awesome!'})
-        expect{op2 = CRM::Account::Create.(account: {name: 'Awesome!'})}.to raise_error(Trailblazer::Operation::InvalidContract)
+    it "should raise a Trailblazer::Operation::InvalidContract on create if an account with the same name exists" do
+      op1 = CRM::Account::Create.(account: {name: 'Awesome!'})
+      expect{op2 = CRM::Account::Create.(account: {name: 'Awesome!'})}.to raise_error(Trailblazer::Operation::InvalidContract)
     end
   end
 
   describe "create an account with an address" do
     it "must create both the account and the address" do
       op = CRM::Account::Create.(account: {name: "Awesome!", shipping_address: {street1: "1 Orchard Rd", city: "Singapore"}})
-      op.model.persisted?.should be(true)
-      op.model.shipping_address.addressable_id.should equal(op.model.id)
+      expect(op.model.persisted?).to be(true)
+      expect(op.model.shipping_address.addressable_id).to eq(op.model.id)
     end
   end
 end
